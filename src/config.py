570a8f9b0bf400a29af557d1f680442eb3cbd0b2
Taehun Kim <kingmbc@gmail.com>
2020-03-10 00:52:07 -0400

    add source

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import transformers
+
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+EPOCHS = 10
+BERT_PATH = "../input/bert-base-uncased/"
+MODEL_PATH = "model.bin"
+TRAINING_FILE = "../input/IMDB Dataset.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BERT_PATH,
+    do_lower_case=True
+)
+LEARNING_RATE = 3e-5
+DEVICE = "cpu"
